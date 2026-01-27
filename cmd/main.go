0bdd9e11d81478769b8b484a2d40b031779d461e
hmdyt <me@hmdyt.dev>
2026-01-27 22:46:22 +0900

    Revert "sample operator"
    This reverts commit c3a6cb29ecaef59c9d612e3488a32b2b42d9f29e.

--- cmd/main.go
+++ cmd/main.go
@@ -34,9 +34,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
-
-	demov1alpha1 "github.com/hmdyt/homelab-gateway-operator/api/v1alpha1"
-	"github.com/hmdyt/homelab-gateway-operator/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -48,7 +45,6 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	utilruntime.Must(demov1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -178,13 +174,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := (&controller.SampleReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Sample")
-		os.Exit(1)
-	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
